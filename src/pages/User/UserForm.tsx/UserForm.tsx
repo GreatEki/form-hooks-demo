99ef7ec0f193a44ecd0f1d7cc8d64b28c337756e
GreatEki <ekeneidiagbor@gmail.com>
2022-12-10 18:37:51 +0100

    chore: using handleSubmit

--- src/pages/User/UserForm.tsx/UserForm.tsx
+++ src/pages/User/UserForm.tsx/UserForm.tsx
@@ -9,15 +9,19 @@ type FormValues = {
   gender: string;
 };
 const UserForm = () => {
-  const { register, watch } = useForm<FormValues>();
+  const { register, handleSubmit } = useForm<FormValues>();
 
+  function onSubmit(data: FormValues) {
+    console.log(data);
+  }
   return (
     <div className={styles.userForm}>
       <h1> User Form</h1>
-      <form>
+      <form onSubmit={handleSubmit((data) => onSubmit(data))}>
         <Input name="firstName" label="FirstName" register={register} />
-        {/* <Input name="lastName" label="Lastname" register={register} />
-        <Input name="" label="Age" register={register} /> */}
+        <Input name="lastName" label="Lastname" register={register} />
+
+        <input type="submit" />
       </form>
     </div>
   );
